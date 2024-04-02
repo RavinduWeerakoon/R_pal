5de77ef648187faaf19a583f9376ada898ec1855
Lakindu Kariyawasam <lckariyawasam@yahoo.com>
2024-04-02 19:03:14 +0530

    Change Queue class

--- queue_.py
+++ queue_.py
@@ -1,13 +1,15 @@
+from token_ import Token
+
 class Queue():
     def __init__(self):
         self.queue = []
-    def enqueue(self, item):
+    def enqueue(self, item) -> None:
         self.queue.append(item)
-    def dequeue(self):
+    def dequeue(self) -> Token:
         return self.queue.pop(0)
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.queue) == 0
-    def peek(self, index=0):
+    def peek(self, index=0) -> Token:
         if len(self.queue) > index + 1:
             return self.queue[index]
         else:
