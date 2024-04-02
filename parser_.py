5f4fa9be93ed1e9340157b263abc11f45d2c3c02
Ravindu Weerakoon <ravinduhiran26@gmail.com>
2024-04-02 19:08:26 +0530

    adding the parser rules

--- parser_.py
+++ parser_.py
@@ -1,5 +1,5 @@
 from token_ import Token
-from queue import Queue
+from queue_ import Queue
 
 class Parser():
 
@@ -55,11 +55,77 @@ def Ew():
     else:
         read(";")
 
-def Vb():
-    pass
 
-def T():
-    pass
+def D():
+    Da()
 
+    if input_stream.peek().value == "within":
+        read("within")
+        print("D -> Da within D")
+        D()
+    
+
+def Da():
+    Dr()
+    if input_stream.peek().value == "and":
+        read("and")
+        Dr()
+        
+        while input_stream.peek().value == "and":
+            read("and")
+            Dr()
+    
 def Dr():
-    pass
+    if input_stream.peek().value =="rec":
+        read("rec")
+        Db()
+    elif input_stream.peek().value == "IDENTIFIER" or input_stream.peek().type == "left_bracket":
+        Db()
+    else:
+        raise Exception("Expected rec or IDENTIFIER or left_bracket but got {input_stream.peek().value}")
+
+def Db():
+    if input_stream.peek().type == "IDENTIFIER":
+        if input_stream.peek(index=1).value == "IDENTIFIER":
+            read("IDENTIFIER")
+            Vb()
+            while input_stream.peek().value == "IDENTIFIER":
+                Vb()
+            read("=")
+            E()
+            print("Db -> Vb+ = E")
+        elif input_stream.peek().value == "=":
+            Vl()
+            read("=")
+            E()
+    elif input_stream.peek().value == "(":
+        read("(")
+        D()
+        read(")")
+        print("Db -> (D)")
+    
+    else:
+        raise Exception("Expected IDENTIFIER or ( but got {input_stream.peek().value}")
+    
+
+def Vb():
+    if input_stream.peek().type == "IDENTIFIER":
+        read("IDENTIFIER")
+    elif input_stream.peek().value == "(":
+        read("(")
+        Vl()
+        read(")")
+    else:
+        raise Exception("Expected IDENTIFIER or ( but got {input_stream.peek().value}")
+    
+def Vl():
+    if input_stream.peek().type == "IDENTIFIER":
+        read("IDENTIFIER")
+        read("list")
+    else:
+        raise Exception("Expected IDENTIFIER but got {input_stream.peek().value}")
+
+
+
+
+        
